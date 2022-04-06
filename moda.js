8b701100dfeef662c8024b0faa82652c7198ed03
dtorres <diego.torres.mt@correo.usa.edu.co>
2022-04-05 22:18:30 -0500

    Moda Calculated without functions

--- moda.js
+++ moda.js
@@ -11,6 +11,20 @@ const lista1Count = {
 // con esta clase podemos recorrer el arreglo,
 lista1.map(
     function (elemento){
-        lista1Count[elemento] = 1;
+        //debemos validar si el elmento ya existe
+        if (lista1Count[elemento]){
+            lista1Count[elemento] += 1;
+        }else {
+            lista1Count[elemento] = 1;
+        }
     }
 );
+//según el ejercicio debemos devolvernos al array
+const  lista1Array = Object.entries(lista1Count).sort(
+    function (valorAcumulado, nuevoValor) {
+        return valorAcumulado[1] - nuevoValor[1];
+    }
+);
+
+const moda = lista1Array[lista1Array.length - 1]
+
